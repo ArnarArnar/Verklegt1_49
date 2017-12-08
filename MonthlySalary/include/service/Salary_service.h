2ac4a238b69e2420e1cf413f5df722669a2bf95e
oddnyk <oddnykaren@gmail.com>
2017-12-08 22:07:57 +0000

    breytinga á repos og service

--- MonthlySalary/include/service/Salary_service.h
+++ MonthlySalary/include/service/Salary_service.h
@@ -1,6 +1,6 @@
 #ifndef Salary_service_h
 #define Salary_service_h
-
+#include <string>
 #include <stdio.h>
 #include "salary_record.h"
 #include "Salary_repository.h"
@@ -13,6 +13,7 @@ class SalaryService {
         void get_salary_for_a_given_year_service (int year_to_look_for);
         void get_total_salary_for_a_given_SSN_service(string SSN_to_look_for);
         void get_name_of_highest_salary_service();
+
     private:
 //    SalaryRepository sal_repo;
 
